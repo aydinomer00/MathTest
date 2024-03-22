9e93a2e1f0cc1833a4055f4d53ca8da720d66fa4
aydinomer00 <aydinomermurat28@gmail.com>
2024-03-22 21:36:17 +0300

    Initial Commit

---
+++ MathTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathTest
+//
+//  Created by Omer Murat Aydin on 22.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
